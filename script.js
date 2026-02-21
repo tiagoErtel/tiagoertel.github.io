9b3e6993e6363c4f78258373ab35ec1905ccca7c
Tiago Ertel <erteltiago0@gmail.com>
2026-02-21 21:07:58 +0000

    projects and contact section

---
+++ script.js
@@ -0,0 +1,45 @@
+const form = document.getElementById("my-form");
+const status = document.getElementById("status");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+  const submitButton = document.getElementById("submit-btn");
+
+  submitButton.disabled = true;
+  submitButton.innerText = "Sending...";
+
+  fetch("https://formspree.io/f/mkovqbpz", {
+    method: "POST",
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks! I'll get back to you soon.";
+        status.style.color = "#38bdf8"; // Your accent color
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML =
+              "Oops! There was a problem submitting your form.";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! Connectivity issue.";
+    })
+    .finally(() => {
+      submitButton.disabled = false;
+      submitButton.innerText = "Send Message";
+    });
+});
